fccde213637b2207ec03d4b55c5b8501fe72ff23
Samuel Juarez <unhearddesert73@hotmail.com>
2023-07-23 23:25:07 -0500

    Adjusted size

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const messages = [
 const createAnswer = () => {
 const answerElement = document.createElement('p')
 
-answerElement.classList.add('fade', 'text-center', 'fs-1', 'fw-bold', 'display-4', 'bg-warning', 'px-2', 'py-5' , 'rounded', 'text-white', 'p-2', 'w-50', 'position-absolute', 'top-50', 'start-50', 'translate-middle')
+answerElement.classList.add('fade', 'text-center', 'fs-3', 'fw-bold', 'display-4', 'bg-warning', 'px-2', 'py-3' , 'rounded', 'text-white', 'w-50', 'position-absolute', 'top-50', 'start-50', 'translate-middle')
 
 const seed = Math.random() * messages.length - 1
 const randomIndex = seed > 0 ? Math.floor(seed) : Math.ceil(seed)
